77f6be0ccf914eb0b057c02d864e92d5a66a26d3
A-Slastikhina <ao.slastikhina@gmail.com>
2022-07-13 16:58:05 +0300

    some mods

--- src/scripts/section_header.js
+++ src/scripts/section_header.js
@@ -2,29 +2,41 @@
 
 let sectionHeader = document.querySelector('.section-header');
 
-// let siteBody = document.querySelector('body');
+const body = document.querySelector('body');
 
-window.addEventListener('scroll', bodyScroll)
-function bodyScroll (){
+ 
+window.addEventListener('scroll', siteScroll)
+function siteScroll (){
     sectionHeader.style.opacity = '0.5';
     sectionHeader.style.backgroundColor = '#FFC64F';
     sectionHeader.style.transition = '0.3s'
 
-}
+};
 
 sectionHeader.addEventListener('mouseover', onHeader)
 function onHeader (){
     sectionHeader.style.opacity = '1';
     sectionHeader.style.backgroundColor = '#111111';
-    sectionHeader.style.transition = '0.3s'
+    sectionHeader.style.transition = '0.3s';
 
 
-}
+};
 
-sectionHeader.addEventListener('mouseout', outHeader)
-function outHeader () {;
-    sectionHeader.style.opacity = 0.5
-    sectionHeader.style.transition = '0.3s'
+sectionHeader.addEventListener('mouseout', outHeader);
+function outHeader () {
+    sectionHeader.style.opacity = 0.5;
+    sectionHeader.style.transition = '0.3s';
 
-}
+};
+
+//let headOffsetTop = sectionHeader.offsetParent;
+//    console.log(headOffsetTop);
+
+    const headTopCoord = sectionHeader.getBoundingClientRect().top;
+    const headTopDocCoord = headTopCoord + document.body.scrollTop;
+
+    console.log(headTopCoord);
+    console.log(headTopDocCoord);
+
+
 
